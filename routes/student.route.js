17da11e7024005bfdc59e4c982dea1435eeabc83
Suliman <sssuliman@bk.ru>
2022-08-29 22:09:53 +0300

    Правочки по названиям файлов

--- routes/student.route.js
+++ routes/student.route.js
@@ -1,6 +1,8 @@
 const { Router } = require("express");
 const router = Router();
-const { student } = require("../controllers/studentController");
+const { student } = require("../controllers/student.controller");
 const authMiddleware = require("../middleware/auth.middleware");
 
 /////Все CRUD операции + authMiddleware
+
+module.exports = router;
